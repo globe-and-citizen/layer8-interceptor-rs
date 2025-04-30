8f4871efd899152292639ea28083c5bcd48027f0
Osoro Bironga <fanosoro@gmail.com>
2025-04-30 12:36:04 +0300

    docs

--- src/js_glue/formdata_polyfill.ts
+++ src/js_glue/formdata_polyfill.ts
@@ -68,5 +68,14 @@ export async function parseFormDataToArray(form: FormData, boundary: String) {
         chunks = temp;
     }
 
+    // Sample output:
+    //
+    //     --AaB03x
+    //     content-disposition: form-data; name="field1"
+    //     content-type: text/plain;charset=windows-1250
+    //     content-transfer-encoding: quoted-printable
+    //
+    //     Joe owes =80100.
+    //     --AaB03x
     return chunks;
 }
