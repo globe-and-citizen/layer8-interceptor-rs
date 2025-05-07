ab3e6b580290f77bb511a3b4656f6cb4ced03c5d
Osoro Bironga <fanosoro@gmail.com>
2025-05-07 07:23:40 +0300

    debug_assertions

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,6 @@ pub(crate) mod js_imports_prelude {
     use crate::js_glue;
     pub use js_glue::js_imports::{clear_expired_cache, console_error, console_log, object_entries, serve_static};
 
-    // #[cfg(debug_assertions)]
+    #[cfg(debug_assertions)]
     pub use js_glue::js_imports::{console_error_, console_log_};
 }
